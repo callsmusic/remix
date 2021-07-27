91fb7a80a4e63e79c9d5fa83252e5eff02477f28
Roj Serbest <rojserbest@icloud.com>
2021-07-27 16:38:20 +0300

    Use end handler

--- src/bot/handlers/groups/index.ts
+++ src/bot/handlers/groups/index.ts
@@ -2,12 +2,14 @@ import { Composer } from "grammy";
 
 import controls from "./controls";
 import stream from "./stream";
+import end from "./end";
 
 const composer = new Composer();
 
 export default composer;
 
 composer
-   .filter((ctx) => !!ctx.chat?.type.includes("group"))
-   .use(controls)
-   .use(stream);
+  .filter((ctx) => !!ctx.chat?.type.includes("group"))
+  .use(controls)
+  .use(stream)
+  .use(end);
