22a62f03cc4d91997fcf37168faf03ceffe75937
Roj Serbest <roj@rojser.best>
2021-08-17 09:47:51 +0300

    Add strings for custom input

--- src/bot/streamer/custom.ts
+++ src/bot/streamer/custom.ts
@@ -2,12 +2,13 @@ import { PassThrough } from "stream";
 import fluent from "fluent-ffmpeg";
 import { User, Message } from "@grammyjs/types";
 import { getMessageUrl } from "../helpers";
+import i18n from "../i18n";
 import { stream } from "./base";
 
 export default async (input: string, message: Message) => {
     return await stream(message.chat.id, {
         url: getMessageUrl(message),
-        title: "Custom Input",
+        title: i18n("custom_input"),
         requester: message.from as User,
         getReadable: async () =>
             fluent(input)
