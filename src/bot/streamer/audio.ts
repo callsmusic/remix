0f75a58d9d473612792caa3039dfc7a75c11b6fa
Roj <ez@roj.im>
2021-12-31 15:03:27 +0300

    Remove unused definition

--- src/bot/streamer/audio.ts
+++ src/bot/streamer/audio.ts
@@ -1,25 +1,24 @@
-import { getFile } from '../helpers/file'
-import { getMessageUrl } from '../helpers/message'
-import convert from '../convert'
-import { stream } from './stream'
-import { Context } from '../context'
+import { getFile } from "../helpers/file";
+import { getMessageUrl } from "../helpers/message";
+import convert from "../convert";
+import { stream } from "./stream";
+import { Context } from "../context";
 
 export default async (
   ctx: Context & {
-    chat: NonNullable<Context['chat']>
-    from: NonNullable<Context['from']>
-    message: NonNullable<Context['message']>
-  }
+    chat: NonNullable<Context["chat"]>;
+    from: NonNullable<Context["from"]>;
+    message: NonNullable<Context["message"]>;
+  },
 ) => {
-  const chatId = ctx.chat.id,
-    audio = ctx.message.audio || ctx.message.reply_to_message?.audio,
+  const audio = ctx.message.audio || ctx.message.reply_to_message?.audio,
     voice = ctx.message.voice || ctx.message.reply_to_message?.voice,
-    fileId = String((audio || voice)?.file_id)
+    fileId = String((audio || voice)?.file_id);
 
   return await stream(ctx, {
     url: getMessageUrl(ctx.message),
-    title: audio ? audio.title || 'Audio file' : 'Voice message',
+    title: audio ? audio.title || "Audio file" : "Voice message",
     requester: ctx.from,
-    getReadable: async () => convert(await getFile(fileId))
-  })
-}
+    getReadable: async () => convert(await getFile(fileId)),
+  });
+};
