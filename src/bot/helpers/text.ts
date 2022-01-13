a02435837aff2e5ea8fcec313a27327f7eaaa0ed
Roj <ez@roj.im>
2022-01-13 13:14:12 +0300

    Remove usage of deprecated function

--- src/bot/helpers/text.ts
+++ src/bot/helpers/text.ts
@@ -6,7 +6,7 @@ export function chunkSubstr(string: string, size: number) {
   const chunks = new Array(numChunks);
 
   for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
-    chunks[i] = string.substr(o, size);
+    chunks[i] = string.substring(o, size);
   }
 
   return chunks;
