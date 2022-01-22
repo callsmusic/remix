24b5d104958ddeba65cdb58e0a911ff253606390
Roj <ez@roj.im>
2022-01-22 12:30:17 +0300

    Fix problems in finish listener (#70)

--- src/userbot.ts
+++ src/userbot.ts
@@ -1,8 +1,6 @@
 import { TelegramClient } from 'telegram'
 import { Logger } from 'telegram/extensions'
 import { StringSession } from 'telegram/sessions'
-import { GramTGCalls } from 'tgcalls-next'
-import { EditParams } from 'tgcalls-next/lib/types'
 import env from './env'
 
 Logger.setLevel('none')
@@ -15,33 +13,5 @@ export const client = new TelegramClient(
     connectionRetries: 10,
   }
 )
-
-class CustomTGCalls extends GramTGCalls {
-  public volume?: number
-
-  editSelf(params: EditParams) {
-    this.volume = params.volume
-    return super.edit(params)
-  }
-}
-
-const instances = new Map<number, CustomTGCalls>()
-
-export function tgcalls(chatId: number, onFinish?: () => void) {
-  const current = instances.get(chatId)
-  if (current) {
-    if (onFinish) {
-      current.removeAllListeners()
-      current.on('finish', onFinish)
-    }
-    return current
-  }
-  const tgcalls = new CustomTGCalls(client, chatId)
-  if (onFinish) {
-    tgcalls.on('finish', onFinish)
-  }
-  instances.set(chatId, tgcalls)
-  return tgcalls
-}
 
 export const start = () => client.start({ botAuthToken: '' })
